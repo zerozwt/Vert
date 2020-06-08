94de6fc120e1f7209618569d55cac5f55421beb4
zerozwt <zerozwt1991@gmail.com>
2020-06-08 22:07:12 +0800

    install script no longer make cert cache dir

--- install/install.sh
+++ install/install.sh
@@ -3,10 +3,7 @@
 INSTALL_DIR=/usr/local/vert
 
 mkdir -p ${INSTALL_DIR}
-mkdir -p ${INSTALL_DIR}/certs
 mkdir -p /tmp/vert
-
-chmod 700 ${INSTALL_DIR}/certs
 
 cp Vert ${INSTALL_DIR}/Vert
 cp conf.yaml ${INSTALL_DIR}/conf.yaml
